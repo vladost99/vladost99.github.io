ba616ac0624e239364e58c21128d859fed80f1b0
vlad <vladsa000@gmail.com>
2020-06-27 00:04:28 +0300

    add new project

---
+++ Course2020/Homework/JavaScript/ToDoList/main.js
@@ -0,0 +1,65 @@
+//елменты блока входа
+const blockLogin = document.querySelector('#login'), //block login
+    btnLogin = document.querySelector('#btnLogin'), //button login
+    email = document.querySelector('#email'), //input email
+    password = document.querySelector('#password'), //input password
+    txtEmail = document.querySelector('#txtEmail'), //text email
+    txtPassw = document.querySelector('#txtPass');  //text password
+
+// елемены блока toDoList
+const blockToDo = document.querySelector('#blockTodo'); //block toDoList
+
+    //при клике проверяет логин и пароль,если правильно скрывает блок
+    btnLogin.addEventListener('click', function() {
+        checkEmail();
+        checkPassword();
+
+      if (checkEmail()  &&  checkPassword()) {
+            blockLogin.style.display = 'none';
+            blockToDo.style.display = 'block';
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Функции для входа на странцицу
+
+    //функция проверяет email
+    function checkEmail() {
+        const emailo = 'testuser@todo.com';
+        if( email.value == emailo) {
+            txtEmail.innerHTML = '';
+            return true;
+        } else {
+            txtEmail.innerHTML = 'Wrong email';
+            txtEmail.style.color = 'red';
+            return false;
+        }
+    }
+
+
+    //функция проверяет пароль
+    function checkPassword() {
+        const parol = '12345678';
+        if(password.value == parol) {
+            txtPassw.innerHTML = '';
+            return true;
+        } else {
+            txtPassw.innerHTML = 'Wrong password';
+            txtPassw.style.color = 'red';
+            return false;
+        }
+    }
+
