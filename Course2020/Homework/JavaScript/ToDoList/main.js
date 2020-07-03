a3957ee1a4bfe921067d28d224f77a3b35f4c356
vlad <vladsa000@gmail.com>
2020-07-03 15:33:08 +0300

    done

--- Course2020/Homework/JavaScript/ToDoList/main.js
+++ Course2020/Homework/JavaScript/ToDoList/main.js
@@ -7,6 +7,20 @@ const blockLogin = document.querySelector('#login'), //block login
     txtPassw = document.querySelector('#txtPass');  //text password
     
 
+// елемены блока toDoList
+const blockToDo = document.querySelector('#blockTodo'),  //block toDoList
+      inputTask = document.querySelector('#addinput'),  //input task
+      btnAddTodo = document.querySelector('#btnAddTask'),  //add task
+      blockFilter = document.querySelector('.blockFilter'); // block filter
+      listToDo = document.querySelector('#todoList'),  //ul list
+      textEdit = document.querySelector('.textEdit'), //text edit
+      searchInput = document.querySelector('#search-input'); // search input
+     
+
+// modal window     
+const modalDelete = document.querySelector('.modal-delete'),  //modal delete
+      modalEdit = document.querySelector('.modal-edit'); // modal edit
+
 
 //при клике проверяет логин и пароль,если правильно скрывает блок
 btnLogin.addEventListener('click', function(e) {
@@ -20,20 +34,7 @@ btnLogin.addEventListener('click', function(e) {
     }
 });
 
-// елемены блока toDoList
-const blockToDo = document.querySelector('#blockTodo'),  //block toDoList
-      inputTask = document.querySelector('#addinput'),  //input task
-      btnAddTodo = document.querySelector('#btnAddTask'),  //add task
-      blockFilter = document.querySelector('.blockFilter'); // block filter
-      listToDo = document.querySelector('#todoList'),  //ul list
-      textEdit = document.querySelector('.textEdit'); //text edit
-      
-      
 
-
-// modal window     
-const modalDelete = document.querySelector('.modal-delete'),  //modal delete
-      modalEdit = document.querySelector('.modal-edit'); // modal edit
 
 
 let list = [];
@@ -44,23 +45,27 @@ if (localStorage.getItem('TODO')) {
 //при нажатие на кнопку додается toDo
 btnAddTodo.addEventListener('click', function() {
     const  date = new Date();
-
-    const   newToDo = {
-        todo: inputTask.value,
-        fullYear: date.getFullYear(),
-        day: date.getDate(),
-        month: date.getMonth(),
-        hours: date.getHours(),
-        minutes: date.getMinutes(),
-        milliseconds: date.getTime(),
-        check: false,
-        priority: 1
+    if(!inputTask.value) {
+        return;
+    }
+    
+    const  newToDo = {
+            todo: inputTask.value,
+            fullYear: date.getFullYear(),
+            day: date.getDate(),
+            month: date.getMonth(),
+            hours: date.getHours(),
+            minutes: date.getMinutes(),
+            milliseconds: date.getTime(),
+            check: false,
+            priority: 1
     };
 
     list.push(newToDo);
     addToDo();
     localStorage.setItem('TODO', JSON.stringify(list));
-
+    inputTask.value = '';
+    
 });
 
 
@@ -69,10 +74,9 @@ listToDo.addEventListener('click', function(event) {
     let id =  event.target.closest('li').getAttribute('id');
    // показывает  модалные окна
     showModals(event.target, id);
+    
     // изменяет значение check
-    if(event.target.classList.contains('fa-check-circle')) {
-        list[id].check = !list[id].check;
-    }
+    check(event.target, id);
 
     // изменение приоритета
     changePriority(event.target, id);
@@ -93,8 +97,8 @@ modalDelete.addEventListener('click', function(event) {
         // удаление toDo из localStorage
         if(event.target.getAttribute('data-delete') == 'yes') {
             if(item.id == id) {
-                list.splice(i, 1); 
                 modalDelete.style.visibility = 'hidden';
+                list.splice(i, 1);
                 
             } 
                 
@@ -111,67 +115,106 @@ modalDelete.addEventListener('click', function(event) {
 // редакт  текста
 modalEdit.addEventListener('click', function(event) {
     let id = this.getAttribute('data-edit');
-   editText(event.target, id);
-   localStorage.setItem('TODO', JSON.stringify(list));
-   addToDo();
+    editText(event.target, id);
+    localStorage.setItem('TODO', JSON.stringify(list));
+    addToDo();
 });
 
 //  при клике сортирует
 blockFilter.addEventListener('click', function(e) {
- 
- sort(e.target);
- localStorage.setItem('TODO', JSON.stringify(list));
- addToDo();
- 
+    sort(e.target);
+    localStorage.setItem('TODO', JSON.stringify(list));
+    addToDo();
 });
+
+// пошук
+searchInput.addEventListener('input', function() {
+   let value = this.value.trim();
+   let liText = document.querySelectorAll('#todoList .textValue');
+   if( value != '') {
+       liText.forEach(item => {
+        if(item.textContent.search(value) == -1) {
+            item.parentElement.classList.add('hide');
+            item.innerHTML = item.innerText;
+        } else {
+            item.parentElement.classList.remove('hide');
+            let str = item.innerText;
+            item.innerHTML = insertMark(str, item.textContent.search(value), value.length);
+        }
+       });
+    }
+    else {
+        liText.forEach(item => {
+            item.parentElement.classList.remove('hide');
+            item.innerHTML = item.innerText;
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+// <!--                    Функции                  -->
+
+
+//подсветка при поиске
+function insertMark(string, pos, len) {
+    return string.slice(0, pos) + '<mark>' + string.slice(pos, pos +len)+ '</mark>' + string.slice(pos + len);
+}
 
 
 // функция сортирует по дате,приоритету,check
 function sort(target) {
     // сортирует по check
     if(target.classList.contains('fa-filter')) {
-        if(target.getAttribute('data-check') == 'false') {
+        if(target.getAttribute('data-check') == 'true') {
             list.sort((a, b) => {
               return  b.check - a.check;
             });
-            target.setAttribute('data-check', 'true');
+            target.setAttribute('data-check', 'false');
         }
          else {
             list.sort((a, b) => {
                 return  a.check - b.check;
             });
-            target.setAttribute('data-check', 'false');
+            target.setAttribute('data-check', 'true');
          }
     }
 
     // sort Priority
     if(target.classList.contains('fa-sort-numeric-down')) {
-        if(target.getAttribute('data-sortprioity') == 'false') {
+        if(target.getAttribute('data-sortprioity') == 'true') {
             list.sort((a, b) => {
               return  a.priority>b.priority?1:-1;
             });
-            target.setAttribute('data-sortprioity', 'true');
+            target.setAttribute('data-sortprioity', 'false');
         }
          else {
             list.sort((a, b) => {
                 return  b.priority>a.priority?1:-1;
             });
-            target.setAttribute('data-sortprioity', 'false');
+            target.setAttribute('data-sortprioity', 'true');
          }
     }
-    // sort Date
+    // sort Date 
     if(target.classList.contains('fa-sort')) {
-        if(target.getAttribute('data-sortdate') == 'false') {
+        if(target.getAttribute('data-sortdate') == 'true') {
             list.sort((a, b) => {
-              return  a.milliseconds>b.milliseconds?1:-1;
+              return  a.milliseconds<b.milliseconds?1:-1;
             });
-            target.setAttribute('data-sortdate', 'true');
+            target.setAttribute('data-sortdate', 'false');
         }
          else {
             list.sort((a, b) => {
-                return  b.milliseconds>a.milliseconds?1:-1;
+                return  a.milliseconds>b.milliseconds?1:-1;
             });
-            target.setAttribute('data-sortdate', 'false');
+            target.setAttribute('data-sortdate', 'true');
          }
     }
 
@@ -180,11 +223,19 @@ function sort(target) {
 
 
 
+// функция для check
+function check(target,id) {
+    if(target.classList.contains('fa-check-circle')) {
+        list[id].check = !list[id].check;
+    }
+}
+
 
 
 //функция редактирут текст
 function editText(target, id) {
     if(target.getAttribute('data-edit') == 'save') {
+        if(!textEdit.value) return;
         list[id].todo = textEdit.value;
         modalEdit.style.visibility = 'hidden';
         modalEdit.removeAttribute('data-edit');
@@ -206,7 +257,7 @@ function showModals(target,id) {
     }
     // модальное окно для редактирование текста
    if(target.classList.contains('fa-edit')) { 
-        textEdit.textContent = '';
+        textEdit.value = list[id].todo;
         modalEdit.setAttribute('data-edit', id);
         modalEdit.style.visibility = 'visible';
     }
