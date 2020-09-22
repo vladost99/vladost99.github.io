5c0da7fe36e4d45733477027463de080f9117308
Vladyslav <vladost000@gmail.com>
2020-09-22 14:07:53 +0300

    update time

--- Yoga/js/main.js
+++ Yoga/js/main.js
@@ -29,7 +29,7 @@ window.addEventListener('DOMContentLoaded', function() {
        });
        
     /*             <!--Timer-->                               */
-    let deadline = '2020-08-21';
+    let deadline = '2020-10-21';
 
     function getTimeRemaining(endtime) {
         let t =  Date.parse(endtime) - Date.parse(new Date),
